39211d90d531faef0f1a11b4cf746656bada178d
Andrew Riad <andrewriad@users.noreply.github.com>
2026-08-09 15:01:23 +0300

    app2: fabricated songs removed, 41 double reads cut, store-legal identity

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerOFFSettings: {
-    serviceWorkerOFFVersion: "1103100632" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerOFFVersion: "906634033" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

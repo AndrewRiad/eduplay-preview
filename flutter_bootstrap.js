73e5dff29bbabb68733174e3902b50f709e5e09e
Andrew Riad <andrewriad@users.noreply.github.com>
2026-08-10 12:57:36 +0300

    app2: language choice persists, stale-UI and hang fixes, offline-first voice

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerOFFSettings: {
-    serviceWorkerOFFVersion: "906634033" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerOFFVersion: "2408592251" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
